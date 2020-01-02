ca2f3c00410f166823465bc70416bf32ab49dd0b
emilio barrera <barrera_emilio@hotmail.com>
2020-01-02 17:04:41 +0000

    Add project files.

---
+++ Xamarin4ShellAppDemo/Xamarin4ShellAppDemo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin4ShellAppDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new MainShell();
+            MainPage = new FlyOutMainShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
